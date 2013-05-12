1f4258f3408a0ac3b85d2587529250368d440aca
dszap <d.s.zapyatoy@gmail.com>
2013-05-12 16:33:47 +0400

    add main method

--- src/java/ru/mpi/oatmeal/Main.java
+++ src/java/ru/mpi/oatmeal/Main.java
@@ -9,6 +9,9 @@ package ru.mpi.oatmeal;
  * @author DSZ
  */
 public class Main {
+    public static void main(String[] args){
+        System.out.println("Oatmeal start");
+    }
     
     public void test (){
         //TEST
