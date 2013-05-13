8e1d20574a540121955b78e20d38d29b8708e61b
Dmitry R <afigelov@yandex.ru>
2013-05-13 21:09:45 +0400

    Test login button reference through managed bean added.

---
+++ src/java/ru/ifmo/sir/oatmeal/Login.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.ifmo.sir.oatmeal;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author D
+ */
+
+@ManagedBean
+public class Login {
+    
+    public String login(){
+        return "sir/video_feature.xhtml";
+    }
+    
+}
