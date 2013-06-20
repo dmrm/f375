2912925bc5121ccc8931c18753dad41853528b09
Dmitry R <afigelov@yandex.ru>
2013-06-20 22:38:44 +0400

    Логаут в темплейт, повещен фильтр, запрещающий авторизованным пользователям переходить на публично доступные страницы(логи, анономная заявка).

---
+++ src/java/ru/ifmo/pe/oatmeal/filters/LoggedRequestsFilter.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.ifmo.pe.oatmeal.filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author D
+ */
+@WebFilter(filterName = "LoggedRequestFilters", urlPatterns = {"/*"})
+public class LoggedRequestsFilter implements Filter {
+    
+    private FilterConfig filterConfig = null;
+    
+    public LoggedRequestsFilter() {
+    }        
+    
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession session = req.getSession(false);
+        String url = req.getRequestURI();
+        
+        if((session != null || req.getRemoteUser() != null) && 
+                (url.contains("login.xhtml") || url.contains("video_feature.xhtml") || 
+                url.contains("loginerr.xhtml") || url.contains("anonymous_request.xhtml"))){
+            res.sendRedirect(req.getContextPath() + "/p/person.xhtml");
+        } else {
+            chain.doFilter(request, response);
+        }        
+    }
+    public void destroy() {    
+        this.filterConfig = null;
+    }
+    public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+}
